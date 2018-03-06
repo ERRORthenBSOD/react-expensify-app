e4112a73e2214764a7efcecead33559aeb57aef6
Atmospheric_b-10 <atmobsod@gmail.com>
2018-03-06 18:14:16 +0300

    try and bsod again

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const path = require('path');
-const express = require ('express');
+const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname,'..' ,'DIST', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, ()=>{
-    console.log("server is up");
+app.listen(port, () => {
+    console.log('Server is up!');
 });
+
