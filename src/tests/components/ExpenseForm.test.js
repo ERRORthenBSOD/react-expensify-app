29f58e21aaa874f7910045e96bb33f9a10cc9ee5
Atmospheric_b-10 <atmobsod@gmail.com>
2018-03-07 12:50:22 +0300

    tests debugged

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -81,13 +81,13 @@ test("should call onSubmit prop for valid form submission", ()=>{
 test("should set new date on date change", ()=>{
     const now = moment();
     const wrapper = shallow(<ExpenseForm />);
-    wrapper.find('withStyles(SingleDatePicker)').prop("onDateChange")(now);
+    wrapper.find('SingleDatePicker').prop("onDateChange")(now);
     expect(wrapper.state('createdAt')).toEqual(now);
 });
 
 test("should onFocusChange", ()=>{
     const wrapper = shallow(<ExpenseForm/>);
     const focused = true;
-    wrapper.find('withStyles(SingleDatePicker)').prop('onFocusChange')({focused});
+    wrapper.find('SingleDatePicker').prop('onFocusChange')({focused});
     expect(wrapper.state('calendarFocused')).toBe(focused);
 });
